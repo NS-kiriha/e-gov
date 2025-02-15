82f6bd62e594fa3fef0251f24209371bc3501ef4
Yagrajoli <ekrajoli1235@gmail.com>
2025-02-15 09:36:37 +0545

    designed hero and ticketing page

---
+++ Pages/ticket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project1.Pages
+{
+    public class ticketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
